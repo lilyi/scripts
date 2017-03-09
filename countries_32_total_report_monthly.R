4912363dafc0370c77542ce96ca139e732de1449
lilyi <lilyli@qnap.com>
2017-03-09 11:48:07 +0800

    without print

--- countries_32_total_report_monthly.R
+++ countries_32_total_report_monthly.R
@@ -41,20 +41,17 @@ tit <- opt$tit
 
 #function to get SESSIONS of 32 COUNTRIES
 myfunction <- function(cname, stime, etime){
-  print(paste(cname,'HI'))
   query.list <- Init(table.id = "ga:3035421", start.date = stime,
                  end.date = etime,
-                 dimensions="ga:date",
-                 metrics="ga:sessions",
-                 sort="ga:date",
+                 dimensions = "ga:date",
+                 metrics = "ga:sessions",
+                 sort = "ga:date",
                  segment = paste("sessions::condition::ga:country=@", cname, sep = ""))
   ga.query <- QueryBuilder(query.list)
   gaData <- GetReportData(ga.query, token)
-  print("datagot") #!
-  write.csv(gaData, paste("Monthly/",tit,"/total_dat/", cname,".csv", sep="")) 
+  write.csv(gaData, paste("Monthly/", tit, "/total_dat/", cname, ".csv", sep="")) 
   gaData$date <- as.Date(gaData$date, format="%Y%m%d") 
   myplot <- myplotF(cname, gaData, gaData$date, gaData$sessions)
-  myplot
   return(myplot)
 }
 myplotF <- function(cname, data, x, y){
@@ -63,23 +60,18 @@ myplotF <- function(cname, data, x, y){
     geom_line(lwd=1, colour = "#0099FF") + 
     scale_x_date(labels = date_format("%m%d")) +
     geom_smooth(method = "lm", formula = y ~ splines::bs(x, 3), se = FALSE, colour = "#9933FF") +
-    labs(title = paste(cname," (",tit," 2017)", sep=""), x = "date", y = "sessions") +
+    labs(title = paste(cname," (", tit, " 2017)", sep=""), x = "date", y = "sessions") +
     theme(axis.text.x = element_text(angle = 0, hjust = 1), title = element_text(size = 10))
-  ggsave(paste("Monthly/",tit,"/total/",cname,".png",sep="")) #!
+  ggsave(paste("Monthly/", tit, "/total/", cname, ".png", sep="")) 
   return(myplot1)
 }
 clist <- list("Australia","Austria","Belgium","Canada","Czechia","Denmark","France","Germany","Greece","Hong Kong","Hungary","India","Iran","Israel","Italy","Japan","Mexico","Netherlands","Norway","Poland","Portugal","Romania","South Africa","South Korea","Spain","Sweden","Switzerland","Taiwan","Thailand","Turkey","United Kingdom","United States")
-#n_clist <- list("Austria","Belgium","Czechia","Denmark","France","Germany","Greece","Hungary","Italy","Netherlands","Norway","Poland","Portugal","Romania","Spain","Sweden","Switzerland","Turkey","United Kingdom","Hong Kong","India","Iran","Israel","Japan","South Korea","Taiwan","Thailand","Canada","Mexico","United States","South Africa","Australia")
 res_sessions32 <- lapply(clist, myfunction, stime, etime)
-#res_sessions32[1]
-#A1 <- do.call(arrangeGrob, c(res_sessions32[1:4], list(ncol=2)))
-#ggsave("C:\\Users\\Lily\\Documents\\GA\\R\\report\\Monthly\\20170201-20170228\\32countrytotal\\01.png", A1)
 i <- seq(from=1, to=32, by=4)
 k <- c(1:8)
 arrangefunction <- function(i,k){
-  title1=textGrob(paste("Total sessions (", tit,". 2017)", sep=""), gp=gpar(fontsize=14, font=2))#fontface="bold")
+  title1 <- textGrob(paste("Total sessions (", tit, ". 2017)", sep=""), gp = gpar(fontsize=14, font=2))#fontface="bold")
   A <- do.call(arrangeGrob, c(res_sessions32[i:(i+3)], list(ncol=2),list(top=title1)))
-  ggsave(paste("Monthly/",tit,"/total/0",k,".png",sep=""), A)
-  return() #!
+  ggsave(paste("Monthly/", tit, "/total/0", k, ".png", sep=""), A)
+  return() 
 }
-#######################try
